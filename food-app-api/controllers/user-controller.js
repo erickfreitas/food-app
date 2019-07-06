e56686bb9171ed68dabb5107cd382e451fa12cf7
Erick <erickfreitasbezerra@gmail.com>
2019-07-06 07:38:38 -0300

    criando autenticação da api

--- food-app-api/controllers/user-controller.js
+++ food-app-api/controllers/user-controller.js
@@ -11,11 +11,11 @@ function userController(){
 }
 
 userController.prototype.get = async (req, res) => {
-    res.status(200).send(await _repository.getAll());
+    res.status(200).send(await controllerBase.get(_repository, req, res));
 };
 
 userController.prototype.getById = async (req, res) => {
-    res.status(200).send(await _repository.getById(req.params.id));
+    res.status(200).send(await controllerBase.getById(_repository, req, res));
 };
 
 userController.prototype.post = async (req, res) => {
@@ -39,7 +39,18 @@ userController.prototype.post = async (req, res) => {
 };
 
 userController.prototype.put = async (req, res) => {
-    res.status(200).send(await _repository.update(req.params.id, req.body));
+    let validationContract = new validation();
+    validationContract.isRequired(req.body.name, 'Informe o nome.');
+    validationContract.isRequired(req.body.email, 'Informe o e-mail.');
+    validationContract.isEmail(req.body.email, 'O e-mail informado é inválido.');
+    validationContract.isRequired(req.params.id, 'Informe o id do usuário que será editado');    
+
+    let userByEmail = _repository.getByEmail(req.body.email);
+    if(userByEmail){
+        validationContract.isTrue(userByEmail && userByEmail.id != req.params.id, `O e-mail ${userByEmail.email} já está em uso.`);
+    }
+
+    controllerBase.put(_repository, validation, req, res);
 };
 
 userController.prototype.delete = async (req, res) => {
