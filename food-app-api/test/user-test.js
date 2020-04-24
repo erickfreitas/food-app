ef39c16cde3a4ebb0412dc8e63c8bd2a484a71bd
Erick Freitas Bezerra <freitas_erick@hotmail.com>
2020-04-24 20:55:44 -0300

    finalizando testes no endpoint de usuários

--- food-app-api/test/user-test.js
+++ food-app-api/test/user-test.js
@@ -12,10 +12,11 @@ chai.use(chaiHttp);
 
 describe('Usuários', () => {
     let token = '';
+    let registeredUserId = '';
 
     before((done) => {
         //removendo usuários testes existente
-        console.log('Removendo usuários de testes')
+        //console.log('Removendo usuários de testes')
         userModel.remove({ name: 'Usuário Mocha'}, () => {
             //testando o cadastro de novo usuário
             let user = new userModel({
@@ -26,7 +27,7 @@ describe('Usuários', () => {
             });        
     
             user.save().then(() => {                
-                console.log('Novo usuário salvo.')
+                //console.log('Novo usuário salvo.')
 
                 //Autenticando usuário na aplicação
                 chai.request(server)
@@ -35,7 +36,7 @@ describe('Usuários', () => {
                     .end((error, response) => {
 
                         token = response.body.token;
-                        console.log(response.body);
+                        //console.log(response.body);
 
                         done();
                     });
@@ -50,7 +51,7 @@ describe('Usuários', () => {
                 .set('authorization', token)
                 .end((error, response) => {
 
-                    console.log(response.body);
+                    //console.log(response.body);
                     response.body.should.be.a('array');
                     response.should.have.status(200);
                     done();
@@ -58,5 +59,90 @@ describe('Usuários', () => {
                 });
         });
     });    
+
+    describe('/POST', () => {
+        it('registrar novo usuário', (done) => {
+            chai.request(server)
+                .post('/api/users/register')
+                .send({
+                    name: 'Usuário Mocha',
+                    email: 'mocha1@gmail.com',
+                    password: 'mocha@123',
+                    confirmPassword: 'mocha@123',
+                    phone: '(00) 000 000 000'
+                })
+                .end((error, response) => {
+
+                    //console.log(response.body);
+                    response.should.have.status(200);
+                    response.body.should.be.a('object');
+                    response.body.should.have.property('email').eq('mocha1@gmail.com');
+                    registeredUserId = response.body._id;
+                    done();
+
+                })
+        });
+
+        it('autenticar o novo usuário', (done) => {
+            chai.request(server)
+                .post('/api/users/authenticate')
+                .send({email: 'mocha1@gmail.com', password: 'mocha@123'})
+                .end((error, response) => {
+
+                    //console.log(response.body);
+                    response.should.have.status(200);
+                    response.body.should.have.a.property('token');
+                    done();
+
+                });
+        });
+
+        it('recuperar dados do usuário cadastrado', (done) => {
+            chai.request(server)
+                .get(`/api/users/${registeredUserId}`)
+                .set('authorization', token)
+                .end((error, response) => {
+
+                    //console.log(response.body);
+                    response.body.should.be.a('object');
+                    response.body.should.have.property('email').eq('mocha1@gmail.com');
+                    done();
+                })
+        });
+    })
+
+    describe('/PUT', () => {
+        it('atualizando nome do usuário', (done) => {
+            chai.request(server)
+                .put(`/api/users/${registeredUserId}`)
+                .set('authorization', token)
+                .send({email: 'mocha1@gmail.com', name: 'Mocha nome | ALTERADO'})
+                .end((error, response) => {
+
+                    //console.log(response.body);
+                    response.should.have.status(200);
+                    response.body.should.be.a('object');
+                    response.body.should.have.property('name').eq('Mocha nome | ALTERADO');
+                    done();
+
+                });
+        });
+    });
+
+    describe('/DELETE', () => {
+        it('deletando usuário', (done) => {
+            chai.request(server)
+                .del(`/api/users/${registeredUserId}`)
+                .set('authorization', token)
+                .end((error, response) => {
+
+                    response.should.have.status(200);
+                    response.body.should.be.a('object');
+                    response.body.should.have.property('message').eq('Register was deleted successfully.');
+                    done();
+                    
+                });
+        });
+    });
 
 });
